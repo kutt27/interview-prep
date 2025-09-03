62b6a1b16708859dbeb01fa957af6e3c91f57402
kutt27 <satheesanamal6@gmail.com>
2025-09-03 19:51:06 +0530

    Added binary search code

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,26 @@
+
+public class BinarySearch{
+    public static int search(int arr1[], int key){
+        int start = 0, end = arr1.length;
+        
+        while (end >= start){
+            int mid = (start + end) / 2;
+
+            if(arr1[mid] == key){
+                return mid;
+            }
+
+            if(arr1[mid] < key){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int key = 8;
+        System.out.println("The key is found at index: " + search(arr1, key));
+    }
+}
