7e5d437ba9ad0bf42f5304a23b8eb2c2300ef8d9
kutt27 <satheesanamal6@gmail.com>
2025-09-19 20:15:54 +0530

    updated the code

--- Arrays/SubArrays.java
+++ Arrays/SubArrays.java
@@ -1,17 +1,44 @@
+import java.util.*;
 public class SubArrays {
+    public static void findMaxMin(int[] arr1){
+        int max = 0;
+        int min = arr1[0]; // assuming the array is sorted
+        for(int i = 0 ; i < arr1.length; i++){
+            if(arr1[i] > max){
+                max = arr1[i];
+            }
+            if(arr1[i] < min){
+                min = arr1[i];
+            }
+        }
+        System.out.println("Maximum sum: " + max);
+        System.out.println("Minimum sum: " + min);
+        
+    }
     public static void printSubArrays(int[] arr){
         int tp = 0;
+        int sum = 0;
+        int arrlength = arr.length;
+        int length = arrlength*(arrlength+1)/2;
+        int arr1[] = new int[length];
+        int number = 0;
         for(int i = 0; i < arr.length; i++){
             for(int j = i; j < arr.length; j++){
                 System.out.print("( ");
                 for(int k = i; k <= j; k++){
                     System.out.print(arr[k] + ",");
+                    number = arr[k];
                 }
+                sum += number;
+                arr1[tp] = sum;
                 tp += 1;
                 System.out.print(")");
                 System.out.println();
             }
+            sum = 0;
         }
+        System.out.println(Arrays.toString(arr1));
+        findMaxMin(arr1);
         System.out.println("Total number of pairs: " + tp);
     }
     public static void main(String[] args) {
@@ -82,4 +109,7 @@ public class SubArrays {
 // ( 9,)
 // ( 9,10,)
 // ( 10,)
+// [1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 2, 5, 9, 14, 20, 27, 35, 44, 54, 3, 7, 12, 18, 25, 33, 42, 52, 4, 9, 15, 22, 30, 39, 49, 5, 11, 18, 26, 35, 45, 6, 13, 21, 30, 40, 7, 15, 24, 34, 8, 17, 27, 9, 19, 10]
+// Maximum sum: 55
+// Minimum sum: 1
 // Total number of pairs: 55
